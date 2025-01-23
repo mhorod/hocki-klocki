71d5d835c4e022bce9267076c78eebd851a123a5
Michal Horodecki <michalhorodecki2002@gmail.com>
2025-01-23 13:43:38 +0100

    Add dropdown with examples

--- webapp/static/main.js
+++ webapp/static/main.js
@@ -1,3 +1,93 @@
+const examples = [
+`
+def main = [X | Y]
+    use -a [X | Y] as RemoveA
+    use +a [X | Y] as AddA
+    link
+        X >-> RemoveA.X,
+        RemoveA.Y >-> AddA.X,
+        AddA.Y >-> Y
+`,
+`
+def main = [X1, X2 | Y]
+    use U{2} [X1, X2 | Y] as U2
+    use +a [X | Y] as AddA
+    link
+        X1 >-> U2.X1, X2 >-> U2.X2,
+        U2.Y >-> AddA.X, 
+        AddA.Y >-> Y
+`,
+`
+def main = [X1, X2 | Y]
+    use U{2} [X1, X2 | Y] as U2
+    use -a [X | Y] as RemoveA
+    link
+        X1 >-> U2.X1, X2 >-> U2.X2,
+        U2.Y >-> RemoveA.X, 
+        RemoveA.Y >-> Y
+`,
+`
+def main = [X | Y]
+    use +a [X | Y] as AddA
+    use -a [X | Y] as RemoveA
+    use U{2} [X1, X2 | Y] as U2
+    link
+        X >-> AddA.X,
+        X >-> RemoveA.X,
+        AddA.Y >-> U2.X1,
+        RemoveA.Y >-> U2.X2,
+        U2.Y >-> Y
+`,
+`
+def main = [X1, X2 | Y]
+    use f [X | Y] as F
+    use g [X | Y] as G
+    use U{2} [X1, X2 | Y] as U2
+    link
+        X1 >-> F.X,
+        X2 >-> G.X,
+        F.Y >-> U2.X1,
+        G.Y >-> U2.X2,
+        U2.Y >-> Y
+        
+def g = [X | Y]
+    use f [X | Y] as F
+    use -b [X | Y] as RemoveB
+    link
+        X >-> F.X,
+        F.Y >-> RemoveB.X,
+        RemoveB.Y >-> Y
+        
+def f = [X | Y]
+    use +a [X | Y] as AddA
+    link
+        X >-> AddA.X,
+        AddA.Y >-> Y
+
+`,
+`
+def f = [X | Y]
+    use +a [X | Y] as AddA
+    use f [X | Y] as F
+    use U{2} [X1, X2 | Y] as U2
+    link
+        X >-> AddA.X,
+        X >-> F.X,
+        AddA.Y >-> U2.X1,
+        F.Y >-> U2.X2,
+        U2.Y >-> Y
+`
+]
+
+function selectExample() {
+    const selection = exampleSelectElement.value;
+    if (selection !== "custom") {
+        const selectionValue = parseInt(selection);
+        codeElement.value = examples[selectionValue];
+    }
+    update()
+}
+
 function update() {
     const code = codeElement.value;
     const expansionDepth = expansionDepthElement.value;
@@ -13,10 +103,10 @@ function update() {
     })
         .then(blob => {
             document.getElementById("codeImage").src = URL.createObjectURL(blob);
-            if(showTyping) {
+            if (showTyping) {
                 console.log("HERERERRE")
                 fetch('/get-typing').then(r => {
-                    if(!r.ok) throw new Error("Non ok return status");
+                    if (!r.ok) throw new Error("Non ok return status");
                     return r.json()
                 }).then(txt => {
                     document.getElementById("typeBox").innerText = txt.ty;
@@ -30,7 +120,11 @@ function update() {
 const codeElement = document.getElementById('codeInput');
 const expansionDepthElement = document.getElementById('expansionDepth');
 const showTypingElement = document.getElementById('showTyping');
+const exampleSelectElement = document.getElementById('exampleSelect');
+
 
 const elements = [codeElement, expansionDepthElement, showTypingElement];
+
+exampleSelectElement.addEventListener('change', selectExample);
 elements.forEach(element => element.addEventListener('input', update));
 
